39da416a00afd48dab513fc6ede8fb73fc68c3a7
Jordan Beasley <jordan.beasley@smail.astate.edu>
2016-11-24 20:07:28 -0600

    Skeleton styling for about me

--- AboutMe.php
+++ AboutMe.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<body>
+  <head>
+  </head>
+  <body>
 
-<h1>About Me</h1>
+  <div class="container-fluid" id="content-container">
+    <div class="col-mid-6" id="pic"></div>
+    <p id="about-container-tags">Jordan Beasley</p>
+    <p id="about-container-tags">Computer Science</p>
+    <p id="about-container-tags">Lorem ipsum dolor sit amet, ei duo harum ludus nemore, an munere nominati recteque per, vix eu sonet disputationi.
+      Mei vocent detracto an, mazim harum ut mea. Elit tritani lobortis vix ut, et sea utinam vocibus, ea est idque argumentum instructior. Sed cu soleat iisque argumentum,
+      ex viris quaeque eam, ex mea quem graeci quaeque. Nam ne eius equidem, cu constituam appellantur duo. Pro ea dolor invidunt, ferri aliquam sapientem ne est.</p>
+
+  </div>
 
 
-</body>
+  </body>
 </html>
